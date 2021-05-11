8d5e1efd71444930632eab71ed516d73e6e373d8
do71730 <80794543+do71730@users.noreply.github.com>
2021-05-11 16:51:16 -0400

    Create Datatypes.c
    https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem

---
+++ C++/Introduction/Easy/Datatypes.c
@@ -0,0 +1,22 @@
+
+include <iostream>
+include <cstdio>
+using namespace std;
+
+int main() {
+    
+    char ch;
+    double d;
+    float f;
+    int i;
+    long ii;
+
+    scanf("%d %ld %c %f %lf", &i,&ii,&ch,&f,&d);
+    printf("%d\n",i);
+    printf(" %ld\n",ii);
+    printf("%c\n",ch);
+    printf("%f\n",f);
+    printf("%lf\n",d);
+    return 0;
+    
+}
