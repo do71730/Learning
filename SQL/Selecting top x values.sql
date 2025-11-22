ed612cd1525d1c66acf60f87bef570ba19d86ed8
laoshilee <do71730@umbc.edu>
2025-11-22 15:49:58 -0500

    Changes from ssms made to local commit from vs

--- SQL/Selecting top x values.sql
+++ SQL/Selecting top x values.sql
@@ -20,5 +20,14 @@ order by customer_id
 offset 5 rows
 fetch first 20 rows only;
 
-select distinct * from bikestores.sales.order_items a
-inner join bikestores.production.products p on a.product_id = b. product_id
+select distinct  top 10  a.product_id,p.product_name,count(order_id) as num_order_with_products from bikestores.sales.order_items a
+inner join bikestores.production.products p on a.product_id = p.product_id
+group by  a.product_id,p.product_name
+order by 3 desc;
+
+select distinct  a.product_id,p.product_name,count(order_id) as num_order_with_products from bikestores.sales.order_items a
+inner join bikestores.production.products p on a.product_id = p.product_id
+group by  a.product_id,p.product_name
+order by 3 desc
+offset 0 row
+fetch first 10 row only
