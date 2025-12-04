ef55b2db6b8148f138347abda3a5d71dca4e22fd
Aleem <80794543+do71730@users.noreply.github.com>
2025-12-04 10:29:04 -0500

    try except

--- python/introduction/easy/Calculator/cal.py
+++ python/introduction/easy/Calculator/cal.py
@@ -1,25 +1,55 @@
 def additon(num1,num2):
-    return num1 + num2
+    result = 0
+    try: 
+        result = num1 + num2
+    except:
+        result = "Error"
+    return result
 
 def subtraction(num1,num2):
-    return num1 - num2
+    result = 0
+    try: 
+        result = num1 - num2
+    except:
+        result = "Error"
+    return result
 
 def division(num1,num2):
-    return num1 / num2
+    result = 0
+    try: 
+        result = num1 / num2
+    except:
+        result = "Error"
+    return result
 
 def multiply(num1,num2):
-    return num1 * num2
+    result = 0
+    try: 
+        result = num1 * num2
+    except:
+        result = "Error"
+    return result
 
 def power(num1,num2):
-    return num1 ** num2
+    result = 0
+    try: 
+        result = num1 ** num2
+    except:
+        result = "Error"
+    return result
 
 def mod(num1, num2):
-    return num1 % num2
+    result = 0
+    try: 
+        result = num1 % num2
+    except:
+        result = "Error"
+    return result
 
 def main():
-    num1 = input("What is the first number: ")
+    num1 = int(input("What is the first number: "))
     opp = input("chose and operation: /,+,-,*,**(power),%(modulus): ")
-    num2 = input("what is the second number: ")
+    num2 = int(input("what is the second number: "))
 
     if(opp == '/'): 
         print(division(num1,num2))
