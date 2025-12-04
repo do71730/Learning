aab0c034f193c3ec8949ad36f6ad89ec44126646
Aleem <80794543+do71730@users.noreply.github.com>
2025-12-04 10:58:46 -0500

    end loop

--- python/introduction/easy/Calculator/cal.py
+++ python/introduction/easy/Calculator/cal.py
@@ -47,24 +47,44 @@ def mod(num1, num2):
     return result
 
 def main():
-    num1 = int(input("What is the first number: "))
-    opp = input("chose and operation: /,+,-,*,**(power),%(modulus): ")
-    num2 = int(input("what is the second number: "))
-
-    if(opp == '/'): 
-        print(division(num1,num2))
-    elif(opp == '*'):
-        print(multiply(num1,num2))
-    elif(opp == '**'):
-        print(power(num1,num2))
-    elif(opp == '-'):
-        print(subtraction(num1,num2))
-    elif(opp == '%'):
-        print(mod(num1,num2))
-    elif(opp == '+'):
-        print(additon(num1,num2))
-    else:
-        print('Wrong operation')
+    n= 'o'
+    while n != 'x':
+        num1 = 0
+        num2 = 0
+        opp = '+'
+        print('enter x at any time to stop: ')
+        num1 = input("What is the first number: ")
+        if num1 != 'x':
+            num1 = int(num1)
+            opp = input("chose and operation: /,+,-,*,**(power),%(modulus): ")
+            if opp != 'x':
+                num2 = input("what is the second number: ")
+                if num2 != 'x':
+                    num2 = int(num2)
+                else: 
+                    n= 'x'
+            else:
+                n ='x'
+        else: 
+            n = 'x'
+        
+        if n != 'x':
+            if(opp == '/'): 
+                print(division(num1,num2))
+            elif(opp == '*'):
+                print(multiply(num1,num2))
+            elif(opp == '**'):
+                print(power(num1,num2))
+            elif(opp == '-'):
+                print(subtraction(num1,num2))
+            elif(opp == '%'):
+                print(mod(num1,num2))
+            elif(opp == '+'):
+                print(additon(num1,num2))
+            else:
+                print('Wrong operation')
+        else:
+            print('Goodbye!')
 
 if __name__ == '__main__':
     main()
