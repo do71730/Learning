493152adba6122ae2512f8d62468bbfd09dab69d
Aleem <80794543+do71730@users.noreply.github.com>
2025-12-04 19:04:06 -0500

    better error handling

--- python/introduction/easy/Calculator/cal.py
+++ python/introduction/easy/Calculator/cal.py
@@ -1,5 +1,5 @@
 '''Takes two numbers and return the sum'''
-def additon(num1,num2):
+def addition(num1,num2):
     result = 0
     try: 
         result = num1 + num2
@@ -62,34 +62,39 @@ def main():
         print('enter x at any time to stop: ')
         num1 = input("What is the first number: ")
         if num1 != 'x':
-            num1 = int(num1)
             opp = input("chose and operation: /,+,-,*,**(power),%(modulus): ")
             if opp != 'x':
                 num2 = input("what is the second number: ")
-                if num2 != 'x':
-                    num2 = int(num2)
-                else: 
-                    n= 'x'
+                if num2 == 'x':
+                    n = 'x'
             else:
                 n ='x'
         else: 
             n = 'x'
         
         if n != 'x':
-            if(opp == '/'): 
-                print(division(num1,num2))
-            elif(opp == '*'):
-                print(multiply(num1,num2))
-            elif(opp == '**'):
-                print(power(num1,num2))
-            elif(opp == '-'):
-                print(subtraction(num1,num2))
-            elif(opp == '%'):
-                print(mod(num1,num2))
-            elif(opp == '+'):
-                print(additon(num1,num2))
-            else:
-                print('Wrong operation')
+            try:
+                num1 = float(num1)
+                num2 = float(num2)
+
+                if(opp == '/'): 
+                    print(division(num1,num2))
+                elif(opp == '*'):
+                    print(multiply(num1,num2))
+                elif(opp == '**'):
+                    print(power(num1,num2))
+                elif(opp == '-'):
+                    print(subtraction(num1,num2))
+                elif(opp == '%'):
+                    print(mod(num1,num2))
+                elif(opp == '+'):
+                    print(addition(num1,num2))
+                else:
+                    print('Wrong operation')
+            except:
+                print('Number Error')
+
+            
         else:
             print('Goodbye!')
 
